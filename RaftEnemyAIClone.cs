c80174eeb67065b35e86e96885ef001084a612c5
Madison Trumble <63116555+madisonmtrumble@users.noreply.github.com>
2021-10-10 18:13:12 -0700

    Add files via upload

---
+++ RaftEnemyAIClone.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaftEnemyAIClone : MonoBehaviour
+{
+    public GameObject idlePos;
+
+    public GameObject attackTarget;
+
+    public GameObject enemy;
+
+    public bool hitTile;
+
+    public List<GameObject> raftTiles = new List<GameObject>();
+    void Start()
+    {
+        raftTiles.AddRange(GameObject.FindGameObjectsWithTag("Tile"));
+
+        enemy = GameObject.Find("Enemy");
+        
+        hitTile = false;
+        Idle();
+    }
+
+    void Idle()
+    {
+        InvokeRepeating("Attack", 2.0f, 3.0f);
+        gameObject.transform.position = idlePos.transform.position;
+        hitTile = false;
+    }
+
+    void Attack()
+    {
+        attackTarget = raftTiles[Random.Range(0, raftTiles.Count)];
+
+        /*
+        if (attackTarget != null)
+        {
+            Debug.Log("Made it this far.");
+            enemy.transform.position = attackTarget.transform.position;
+            hitTile = true;
+        }
+        else
+        {
+            Debug.Log("No selected target...");
+        }
+        
+        if(hitTile == true)
+        {
+            Idle();
+        }
+        */
+        
+    }
+    void Update()
+    {
+        enemy.transform.position = attackTarget.transform.position;
+
+        raftTiles.RemoveAll(item => item == null);
+    }
+}
---
+++ tileHealth.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tileHealth : MonoBehaviour
+{
+    public int health = 3;
+
+    public GameObject enemy;
+
+    public GameObject tile;
+    void Start()
+    {
+        enemy = GameObject.Find("Enemy");
+        tile = GameObject.Find("Tile");
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject == enemy && health >= 2)
+        {
+            health--;
+            Debug.Log("Enemy hit Tile.");
+        }
+        if(collision.gameObject == enemy && health == 1)
+        {
+            health--;
+            Destroy(tile);
+            Debug.Log("Enemy destroyed Tile.");
+            
+        }
+    }
+    void Update()
+    {
+        
+    }
+}
